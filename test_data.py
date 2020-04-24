2d0f7ebdf26194b47fb9cbbe8fd982c7eb3e7699
pjayaraj <pjayaraj@github.com>
2020-04-25 00:47:27 +0530

    Added NZ, tweaked squad selection

--- test_data.py
+++ test_data.py
@@ -5,26 +5,27 @@ from players import*
 #set teams here
 ind_squad = [sachin, sehwag, gambhir, kohli, yuvraj, dhoni, raina, ashwin, zaheer, nehra, sreesanth]
 ind_squad[0].onstrike=True
-opening_pair_ind = [ind_squad[0],ind_squad[1]]
-team_ind=Team(ind_squad, 0, False, False, 0, "india", 0, 0, opening_pair_ind)
+team_ind=Team(ind_squad, 0, False, False, 0, "india", 0, 0, [ind_squad[0],ind_squad[1]])
 
 #has to be odd numbered
 aus_squad = [hayden, gilchrist, ponting, clarke, hussey, watson, white, hogg, johnson, lee, tait]
 aus_squad[0].onstrike=True
-opening_pair_aus = [aus_squad[0], aus_squad[1]]
-team_aus=Team(aus_squad, 0, False, False, 0, "australia", 0, 0, opening_pair_aus)
+team_aus=Team(aus_squad, 0, False, False, 0, "australia", 0, 0, [aus_squad[0], aus_squad[1]])
 
 #has to be odd numbered
 sl_squad = [tharanga, dilshan, sangakkara, jayawardene, samaraweera, kapugedera, kulashekara, perera, randiv, malinga, murali]
 sl_squad[0].onstrike=True
-opening_pair_sl = [sl_squad[0], sl_squad[1]]
-team_sl=Team(sl_squad, 0, False, False, 0, "sri lanka", 0, 0, opening_pair_sl)
+team_sl=Team(sl_squad, 0, False, False, 0, "sri lanka", 0, 0, [sl_squad[0], sl_squad[1]])
 
 #rsa
 sa_squad = [smith,amla,kallis,devilliers,duminy,duplessis,vanvyk,botha,peterson,morkel,steyn]
 sa_squad[0].onstrike=True
-opening_pair_sa=[sa_squad[0],sa_squad[1]]
-team_sa=Team(sa_squad, 0, False, False, 0, "south africa", 0, 0, opening_pair_sa)
+team_sa=Team(sa_squad, 0, False, False, 0, "south africa", 0, 0, [sa_squad[0],sa_squad[1]])
 
-list_of_teams = [team_sl, team_aus, team_ind, team_sa]
+#nz
+nz_squad = [guptill,bmccullum,how,taylor,franklin,styris,nmccullum,oram,mills,vettori,southee]
+nz_squad[0].onstrike=True
+team_nz=Team(nz_squad, 0, False, False, 0, "new zealand", 0, 0, [nz_squad[0],nz_squad[1]])
+
+list_of_teams = [team_sl, team_aus, team_ind, team_sa, team_nz]
 team_names=[l.name for l in list_of_teams]
