fda6f5db349245498cb5da4a37e04dc1d6963af8
pjayaraj1989 <33594359+pjayaraj1989@users.noreply.github.com>
2020-04-24 21:50:48 +0530

    Minor edits to main

--- BookCricket.py
+++ BookCricket.py
@@ -3,24 +3,18 @@ from test_data import*
 
 if __name__ == "__main__": 
     #start innings
-    team_ind = Team(india_squad, 0, False, False, 0, "India", 0, 0)
-    #first member always on strike
-    opening_pair_ind = [sachin, sehwag]
-    sachin.onstrike=True
-    Play(team_ind, extras_ind, opening_pair_ind)
+    t1 = Team(india_squad, 0, False, False, 0, "India", 0, 0)   
+    Play(t1, extras_ind, opening_pair_ind)
+    print("End of 1st innings")
     target=0
-    target = team_ind.total_score + 1        
-    
+    target = t1.total_score + 1        
+    print("Target: " + str(target))
+    input()
     #start innings
-    team_aus = Team(australia_squad, 0, False, True, target, "Australia", 0, 0)
-    #first member always on strike
-    opening_pair_aus = [hayden, gilchrist]
-    gilchrist.onstrike=True
-    Play(team_aus, extras_aus, opening_pair_aus)
-
+    t2 = Team(australia_squad, 0, False, True, target, "Australia", 0, 0)    
+    Play(t2, extras_aus, opening_pair_aus)
     #now check scores
-    result = CalculateResult(team_ind, team_aus)
-
+    result = CalculateResult(t1, t2)
     print("Summary:")
     print(result.team1.name + " " + 
           str(result.team1.total_score) + "/" + 
