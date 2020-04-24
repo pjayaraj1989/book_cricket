3a9c1995eca4abdec3aa42306268fc62d35ac6af
pjayaraj1989 <33594359+pjayaraj1989@users.noreply.github.com>
2020-04-24 16:28:29 +0530

    Updated main with extras, wickets, team members

--- BookCricket.py
+++ BookCricket.py
@@ -2,11 +2,16 @@ from Resources import*
 
 if __name__ == "__main__":
     balls=10
-    sachin=Player('Sachin',False, 0, 0)
-    sehwag=Player('Sehwag',False, 0, 0)
+    sachin=Player('Sachin',False, 0, 0, True)
+    sehwag=Player('Sehwag',False, 0, 0, True)
+    gambhir=Player('Gambhir',False, 0, 0, True)
+    extras = Player('Extra',False,0,0,True)
+
+    team = [sachin, sehwag, gambhir]
 
     #first member always on strike
     pair = [sachin, sehwag]
     sachin.onstrike=True
 
-    Play(pair)
+    Play(team,extras, pair)
+
