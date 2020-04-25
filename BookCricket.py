7373f8a90a28e6f0e43c471d28aa78d25b941376
pjayaraj1989 <33594359+pjayaraj1989@users.noreply.github.com>
2020-04-25 13:23:24 +0530

    Updated main with new refactored classes

--- BookCricket.py
+++ BookCricket.py
@@ -3,13 +3,13 @@ from test_data import*
 from helper import*
 
 if __name__ == "__main__":
-    match=GetMatchInfo()
-    
+    match=GetMatchInfo()    
     balls=int(match.overs)*6
     
     t1=match.team1
     t2=match.team2
-    
+
+    #first innings
     Play(t1, extras, t1.opening_pair, balls)
     target=0
     target = t1.total_score + 1        
@@ -26,5 +26,5 @@ if __name__ == "__main__":
     
     #add result to match
     match.result = result
-    
+
     PrintResult(match.result)
