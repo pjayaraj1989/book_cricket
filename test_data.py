4a3ad86163bb94996ac24a607342344dfa9c29f9
pjayaraj1989 <33594359+pjayaraj1989@users.noreply.github.com>
2020-04-25 13:22:50 +0530

    Updated with refactored classes

--- test_data.py
+++ test_data.py
@@ -5,27 +5,16 @@ from players import*
 #set teams here
 ind_squad = [sachin, sehwag, gambhir, kohli, yuvraj, dhoni, raina, ashwin, zaheer, nehra, sreesanth]
 ind_squad[0].onstrike=True
-team_ind=Team(ind_squad, 0, False, False, 0, "india", 0, 0, [ind_squad[0],ind_squad[1]])
+team_ind=Team(team_array=ind_squad,
+                name="India", 
+                opening_pair=[ind_squad[0],ind_squad[1]])
 
 #has to be odd numbered
 aus_squad = [hayden, gilchrist, ponting, clarke, hussey, watson, white, hogg, johnson, lee, tait]
 aus_squad[0].onstrike=True
-team_aus=Team(aus_squad, 0, False, False, 0, "australia", 0, 0, [aus_squad[0], aus_squad[1]])
+team_aus=Team(team_array=aus_squad,
+                    name="Australia",
+                    opening_pair=[aus_squad[0],aus_squad[1]])
 
-#has to be odd numbered
-sl_squad = [tharanga, dilshan, sangakkara, jayawardene, samaraweera, kapugedera, kulashekara, perera, randiv, malinga, murali]
-sl_squad[0].onstrike=True
-team_sl=Team(sl_squad, 0, False, False, 0, "sri lanka", 0, 0, [sl_squad[0], sl_squad[1]])
-
-#rsa
-sa_squad = [smith,amla,kallis,devilliers,duminy,duplessis,vanvyk,botha,peterson,morkel,steyn]
-sa_squad[0].onstrike=True
-team_sa=Team(sa_squad, 0, False, False, 0, "south africa", 0, 0, [sa_squad[0],sa_squad[1]])
-
-#nz
-nz_squad = [guptill,bmccullum,how,taylor,franklin,styris,nmccullum,oram,mills,vettori,southee]
-nz_squad[0].onstrike=True
-team_nz=Team(nz_squad, 0, False, False, 0, "new zealand", 0, 0, [nz_squad[0],nz_squad[1]])
-
-list_of_teams = [team_sl, team_aus, team_ind, team_sa, team_nz]
+list_of_teams = [team_aus, team_ind,]
 team_names=[l.name for l in list_of_teams]
