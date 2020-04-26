db61514773c00c57021c6252f4ef7ed615eef015
pjayaraj1989 <33594359+pjayaraj1989@users.noreply.github.com>
2020-04-26 09:46:29 +0530

    Updated main file with calculating results

--- BookCricket.py
+++ BookCricket.py
@@ -9,22 +9,15 @@ if __name__ == "__main__":
     t2=match.team2
     
     Play(t1, t2, t1.opening_pair, balls)
+    DisplayScore(t1) 
+    input()
+    target=t1.total_score+1
 
-    
-    '''
-    #first innings
-    Play(t1, extras, t1.opening_pair, balls, t2)
-    target=0
-    target = t1.total_score + 1        
-    input('End of inns. Target: {0}'.format(str(target)))    
-    #second innings
+    t2.target = target
     t2.batting_second=True
-    t2.target=target
-    extras.runs=0
-    Play(t2, extras, t2.opening_pair, balls, t1)    
-    #now calc result
-    result = CalculateResult(t1, t2)    
-    #add result to match
+    Play(t2, t1, t2.opening_pair, balls)
+    DisplayScore(t2)
+    
+    result = CalculateResult(t1, t2)
     match.result = result
     PrintResult(match.result)
-    '''
