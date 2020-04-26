3bf77d8f9ad5a5135a94710ca0e2f2746186312b
pjayaraj1989 <33594359+pjayaraj1989@users.noreply.github.com>
2020-04-26 20:43:25 +0530

    Lot of changes

--- BookCricket.py
+++ BookCricket.py
@@ -4,20 +4,25 @@ from helper import*
 
 if __name__ == "__main__":
     match=GetMatchInfo()    
-    balls=int(match.overs)*6    
+    overs=match.overs  
     t1=match.team1
     t2=match.team2
     
-    Play(t1, t2, t1.opening_pair, balls)
+    bowlers_t1 = [plr for plr in t1.team_array if plr.attr.bowling > 5]
+    bowlers_t2 = [plr for plr in t2.team_array if plr.attr.bowling > 5]
+
+    Play(t1, t2, t1.opening_pair, overs, bowlers_t2)
     DisplayScore(t1) 
+    DisplayBowlingStats(bowlers_t2)
     input()
     target=t1.total_score+1
 
     t2.target = target
     t2.batting_second=True
-    Play(t2, t1, t2.opening_pair, balls)
+    Play(t2, t1, t2.opening_pair, overs, bowlers_t1)
     DisplayScore(t2)
-    
+    DisplayBowlingStats(bowlers_t1)
+
     result = CalculateResult(t1, t2)
     match.result = result
     PrintResult(match.result)
