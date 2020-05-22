e4651deac32cbe516ed498c429638f393728ac7c
pjayaraj1989 <33594359+pjayaraj1989@users.noreply.github.com>
2020-05-22 17:00:01 +0530

    Minor fix in installer

--- Install.cmd
+++ Install.cmd
@@ -1,6 +1,6 @@
 ::for windows users, run this first time, then run Run.cmd
 ::install python3.8
-python-3.8.2.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+.\python-3.8.3-amd64.exe /quiet InstallAllUsers=1 PrependPath=0 Include_test=0
 ::run script to install pip utility
 python3 get-pip.py
 ::this pkg is required to color the console
