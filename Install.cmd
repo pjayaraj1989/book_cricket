6f43fdc0a010f691d0d4d1b3e0be1ad857454ba5
pjayaraj1989 <33594359+pjayaraj1989@users.noreply.github.com>
2020-05-24 18:12:35 +0530

    Removed unwanted files for windows release

--- Install.cmd
+++
@@ -1,7 +0,0 @@
-::for windows users, run this first time, then run Run.cmd
-::install python3.8
-.\python-3.8.3-amd64.exe /quiet InstallAllUsers=1 PrependPath=0 Include_test=0
-::run script to install pip utility
-python3 get-pip.py
-::this pkg is required to color the console
-python3 -m pip install colorama
--- Run.cmd
+++
@@ -1,2 +0,0 @@
-::run the main script
-BookCricket.py
